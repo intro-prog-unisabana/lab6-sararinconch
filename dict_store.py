f922316670665128e7fe7ce74d18c798abd1c2f7
Sara Rincón <sararich@unisabana.edu.co>
2026-04-02 11:17:17 -0500

    Problema 2

--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,9 @@
-# Write your code here!
+diccionario = {"temp": 22.5, "color": "blue", "status": "ok"}
+def temp_and_color(diccionario):
+    if "temp" in diccionario and "color" in diccionario:
+        return (diccionario["temp"], diccionario["color"])
+    else:
+        return (None, None)
+    
+resultado = temp_and_color(diccionario)
+print(resultado)
