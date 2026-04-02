257071fcd0985a98e059e4c0619a23f2a379c311
Sara Rincón <sararich@unisabana.edu.co>
2026-04-02 12:51:06 -0500

    Problema 3

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,23 @@
-# Write your code here!
+información = {
+    "Name": "Diego",
+    "Salary": 5000000,
+    "Role": "Janitor",
+    "Years at company": 9001,
+    "Hours per week": 2
+}
+def employee_print(información):
+    nombre = información.get("Name", "N/A")
+    salario = información.get("Salary", "N/A" )
+    rol = información.get("Role", "N/A" )
+    print("Name:", nombre)
+    print("Salary:", salario)
+    print("Role:", rol)
+    del información["Name"]
+    del información["Salary"]
+    del información["Role"]
+    nuevo = información
+    if not nuevo:
+        print("No other info!")
+    else:
+        for key, value in nuevo.items():
+            print(f"{key}: {value}")
