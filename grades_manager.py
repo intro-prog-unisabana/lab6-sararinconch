fd73de40bee770ebd942d0b78ea0c87bdf4eaff5
Sara Rincón <sararich@unisabana.edu.co>
2026-04-03 12:26:02 -0500

    Problema 6 - Parte II

--- grades_manager.py
+++ grades_manager.py
@@ -19,3 +19,22 @@ def initialize_dict(student_name, subject_grades):
     new_dict = {student_name: subject_grades}
     return new_dict
 
+def add_student(student_grades = {}):
+    nombre = input("Enter student name:")
+    nombre_tit = nombre.title()
+    sub_gra = (input("Enter subject and grade (or 'exit' to finish):")).lower().strip()
+    estudiante = {}
+    while sub_gra != "exit":
+        posición_coma = sub_gra.find(",")
+        materia = (sub_gra[0:posición_coma]).title()
+        nota = float(sub_gra[posición_coma+1:])
+        sub_gra = (input("Enter subject and grade (or 'exit' to finish):")).lower().strip()
+        estudiante[materia] = nota
+        student_grades[nombre_tit] = estudiante
+    return student_grades
+
+
+
+
+
+
